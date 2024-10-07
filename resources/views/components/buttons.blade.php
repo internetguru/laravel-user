34fd496b936d046d4c67d0243bde409593fde0e4
petrzpav <pavel.petrzela@internetguru.cz>
2024-10-07 10:40:47 +0200

    Add buttons classes

--- resources/views/components/buttons.blade.php
+++ resources/views/components/buttons.blade.php
@@ -10,7 +10,7 @@
 }">
     <div class="socialite-{{ $action }}">
         @foreach ($providers as $provider)
-            <a class="btn btn-primary" x-bind:href="`{{
+            <a class="btn btn-primary btn-socialite-{{ $provider->value }}" x-bind:href="`{{
                 route('socialite.action', ['provider' => $provider, 'action' => $action])
             }}?remember=${remember}&prev_url={{ $prev_url }}`">
                 <i class="{{ config("services.{$provider->value}.icon") }}"></i>
