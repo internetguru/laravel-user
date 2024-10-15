f2667295bbbc6ac9849ddc11a5179fe2fce3750f
petrzpav <pavel.petrzela@internetguru.cz>
2024-10-15 15:06:36 +0200

    Separate token auth routes from socialite routes

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,24 @@ Route::controller(SocialiteController::class)
     ->middleware('web')
     ->group(function () {
 
-        Route::get('/token-auth/{token}', 'handleTokenAuthCallback')
-            ->middleware('signed')
-            ->name('socialite.token-auth.callback');
-
-        Route::get('/token-auth/{user}', 'handleTokenAuthSend')
-            ->name('socialite.token-auth.send');
-
         Route::get('/{provider}/{action}', 'handleProviderAction')
             ->name('socialite.action');
 
         Route::get('/{provider}/{action}/callback', 'handleProviderCallback')
             ->name('socialite.callback');
+
+    });
+
+Route::controller(SocialiteController::class)
+    ->prefix('token-auth')
+    ->middleware('web')
+    ->group(function () {
+
+        Route::get('/send/{user}', 'handleTokenAuthSend')
+            ->name('socialite.token-auth.send');
+
+        Route::get('/callback/{token}', 'handleTokenAuthCallback')
+            ->middleware('signed')
+            ->name('socialite.token-auth.callback');
 
     });
