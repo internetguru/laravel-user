fdbcaab56d728702f3c668d28e983fb52f594506
petrzpav <pavel.petrzela@internetguru.cz>
2024-10-15 15:00:27 +0200

    Fix routes order

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,18 @@ Route::controller(SocialiteController::class)
     ->prefix('socialite')
     ->middleware('web')
     ->group(function () {
-        Route::get('/{provider}/{action}', 'handleProviderAction')
-            ->name('socialite.action');
-        Route::get('/{provider}/{action}/callback', 'handleProviderCallback')
-            ->name('socialite.callback');
+
         Route::get('/token-auth/{token}', 'handleTokenAuthCallback')
             ->middleware('signed')
             ->name('socialite.token-auth.callback');
+
         Route::get('/token-auth/{user}', 'handleTokenAuthSend')
             ->name('socialite.token-auth.send');
+
+        Route::get('/{provider}/{action}', 'handleProviderAction')
+            ->name('socialite.action');
+
+        Route::get('/{provider}/{action}/callback', 'handleProviderCallback')
+            ->name('socialite.callback');
+
     });
