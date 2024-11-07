5ccb118a2dbc30688cacf611a905240e365098a5
petrzpav <pavel.petrzela@internetguru.cz>
2024-11-07 08:49:21 +0100

    Update base

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,10 +1,11 @@
 @props([
     'view',
     'props' => [],
+    'componentName' => "auth::{{ $view }}"
 ])
 
 <x-dynamic-component
-    component="auth::{{ $view }}"
+    :component="$componentName"
     @foreach ($props as $key => $value)
         {{ $key }}="{{ $value }}"
     @endforeach
