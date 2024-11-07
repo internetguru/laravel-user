e465e7cb195b21292e1ea074a9dbb0befe7ba013
petrzpav <pavel.petrzela@internetguru.cz>
2024-11-07 10:17:21 +0100

    Fix register blade

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,7 +2,7 @@
     <div class="row row-basic row-stretched">
         <div class="card card-register">
             <h2 class="display-6">@lang('ig-user::auth.register.title')</h2>
-            <x-socialite::buttons action="register" :showRemember="true" />
+            <x-ig-user::buttons action="register" :showRemember="true" />
             <p class="mt-3 mb-0 text-end"><a href="{{ route('login') }}">@lang('ig-user::auth.back') {{ strtolower(__('ig-user::auth.login.title')) }}</a></p>
         </div>
     </div>
