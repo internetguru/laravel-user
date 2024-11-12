2e8539245476d74febf15dfc26352cd2da3b73fb
petrzpav <pavel.petrzela@internetguru.cz>
2024-11-12 17:39:14 +0100

    Fix login link translations

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,8 +12,8 @@
             <div class="card card-login">
                 <h2 class="display-6">@lang('ig-user::auth.login.title')</h2>
                 <x-ig-user::buttons action="login" :showRemember="true" />
-                <p class="mt-3 mb-0 text-end"><a href="{{ route('token_auth') }}">@lang('ig-user::auth.token_auth.title')</a></p>
-                <p class="mt-1 mb-0 text-end"><a href="{{ route('register') }}">@lang('ig-user::auth.register.title')</a></p>
+                <p class="mt-3 mb-0 text-end"><a href="{{ route('token_auth') }}">@lang('ig-user::layouts.token-auth.title')</a></p>
+                <p class="mt-1 mb-0 text-end"><a href="{{ route('register') }}">@lang('ig-user::layouts.register.title')</a></p>
             </div>
         @endif
     </div>
