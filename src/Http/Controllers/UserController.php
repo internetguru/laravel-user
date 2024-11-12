366054e752cafb6b1ddbe8e4da7cca99a4309a17
petrzpav <pavel.petrzela@internetguru.cz>
2024-11-12 08:27:18 +0100

    Add missing Rule use

--- src/Http/Controllers/UserController.php
+++ src/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
 use InternetGuru\LaravelUser\Enums\Role;
 
 class UserController extends Controller
