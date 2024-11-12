dd09bdbb3f26d5524a674562f37a6fb15ab349aa
petrzpav <pavel.petrzela@internetguru.cz>
2024-11-12 08:03:48 +0100

    Fix ucfirst argument

--- resources/views/components/buttons.blade.php
+++ resources/views/components/buttons.blade.php
@@ -14,7 +14,7 @@
                 route('socialite.action', ['provider' => $provider, 'action' => $action])
             }}?remember=${remember}&prev_url={{ $prev_url }}`">
                 <i class="{{ config("services.{$provider->value}.icon") }}"></i>
-                {{ ucfirst($provider) }}
+                {{ ucfirst($provider->value) }}
             </a>
         @endforeach
     </div>
