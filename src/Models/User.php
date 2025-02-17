18e2e2af64ab8b10bbde1d9eab62c4e2a51f29de
petrzpav <pavel.petrzela@internetguru.cz>
2025-02-17 06:40:49 +0100

    Fix User role cast

--- src/Models/User.php
+++ src/Models/User.php
@@ -37,7 +37,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected function casts(): array
     {
         return [
-            'role' => User::roles(),
+            'role' => static::roles(),
         ];
     }
 
