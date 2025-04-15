66731bed1d8802d4990d790dfe0b4ddc245dac4b
petrzpav <pavel.petrzela@internetguru.cz>
2025-04-15 07:16:29 +0200

    Disable editable on login forms

--- resources/views/register-email.blade.php
+++ resources/views/register-email.blade.php
@@ -2,7 +2,7 @@
     <div class="row row-basic row-stretched">
         <div class="card card-register">
             <h2 class="display-6">@lang('ig-user::auth.register-email.title')</h2>
-            <x-ig::form action="register-email" :route="route('register.email.handle')">
+            <x-ig::form action="register-email" :route="route('register.email.handle')" class="editable-skip">
                 <x-ig::input type="text" name="name" required>@lang('ig-user::auth.register-email.name')</x-ig::input>
                 <x-ig::input type="email" name="email" required>@lang('ig-user::auth.register-email.email')</x-ig::input>
                 <x-ig::submit>@lang('ig-user::auth.register-email.submit')</x-ig::submit>
