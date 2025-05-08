67245a2945fb18ef8429d9c9da3e41a09ef4d35f
petrzpav <pavel.petrzela@internetguru.cz>
2025-05-08 08:34:08 +0200

    Allow configure service enabled by [SERVICE]_OAUTH_ENABLED env

--- config/services.php
+++ config/services.php
@@ -7,6 +7,7 @@ return [
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI'),
         'icon' => 'fa-brands fa-google',
+        'enabled' => env('GOOGLE_OAUTH_ENABLED', true),
     ],
 
     'facebook' => [
@@ -14,6 +15,7 @@ return [
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => env('FACEBOOK_REDIRECT_URI'),
         'icon' => 'fa-brands fa-facebook',
+        'enabled' => env('FACEBOOK_OAUTH_ENABLED', true),
     ],
 
     'seznam' => [
@@ -21,6 +23,7 @@ return [
         'client_secret' => env('SEZNAM_CLIENT_SECRET'),
         'redirect' => env('SEZNAM_REDIRECT_URI'),
         'icon' => 'fa-solid fa-s',
+        'enabled' => env('SEZNAM_OAUTH_ENABLED', false),
     ],
 
 ];
