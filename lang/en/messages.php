f5f6f16ef4c95505136b5cec639c438030eba585
George <george@internetguru.io>
2025-09-17 09:26:54 +0200

    Proofread en translations

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -8,21 +8,21 @@ return [
 
     'disconnect.success' => 'Identity unlinked from your account.',
 
-    'login.forbidden' => 'Action not available for logged users.',
-    'login.notfound' => 'Unable to login: email not found.',
-    'identity.notfound' => 'Unable to login: identity not found.',
-    'login.required' => 'This action requires login.',
+    'login.forbidden' => 'Action not available for logged-in users.',
+    'login.notfound' => 'Unable to log in: email not found.',
+    'identity.notfound' => 'Unable to log in: identity not found.',
+    'login.required' => 'This action requires you to log in.',
     'login.success' => 'Logged in as :name.',
 
     'register.exists' => 'Registration failed: identity or email already registered.',
     'register.success' => 'Registered as :name.',
-    'register.token-auth.success' => 'Account created. Login link sent to email.',
+    'register.token-auth.success' => 'Account created. Login link sent to your email.',
 
     'remember_me' => 'Remember me',
 
     'transfer.notfound' => 'Unable to transfer: identity not found.',
     'transfer.success' => 'Identity transferred.',
 
-    'unexpected' => 'Unexpected error occurred. Please try again later.',
+    'unexpected' => 'An unexpected error occurred. Please try again later.',
 
 ];
--- lang/en/token_auth.php
+++ lang/en/token_auth.php
@@ -2,12 +2,12 @@
 
 return [
 
-    'sent' => 'Login link sent to given email.',
+    'sent' => 'Login link sent to the given email.',
     'wait' => 'Please wait before requesting a new one-time login link.',
     'invalid' => 'Invalid or expired token.',
     'subject' => 'Your One-Time Login',
     'action' => 'Log In Now',
-    'intro' => 'Click below to securely log in with your one-time link.',
+    'intro' => 'Click below to securely log in using your one-time link.',
     'expires' => 'This link will expire soon. If you didn’t request it, you can safely ignore this email.',
 
 ];
