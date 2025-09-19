4bd0646e6d6bdabc953c34fb61160acab12e85e2
George <george@internetguru.io>
2025-09-19 16:55:36 +0200

    Fix email blades

--- resources/views/emails/token_auth-html.blade.php
+++ resources/views/emails/token_auth-html.blade.php
@@ -1,18 +1,17 @@
 @extends('ig-common::layouts.email-html')
-
 @php
-$buttonText = __('ig-user::token_auth.action');
+    $buttonText = __('ig-user::token_auth.action');
 @endphp
 
 @section('content')
-<p>@lang('ig-common::messages.email.hello')<br>@lang('ig-user::token_auth.intro')</p>
+<p>@lang('ig-user::token_auth.intro')</p>
 
 <x-ig-common::emails.button-html :link="$url">{{ $buttonText }}</x-ig-common::emails.button-html>
 
-<p>@lang('ig-user::token_auth.expires', ['expires' => $expires])</p>
+<p>@lang('ig-user::token_auth.expires')</p>
 @endsection
 
 @section('footer')
-@parent
 <x-ig-common::emails.subcopy-html :link="$url" :text="$buttonText" />
+@parent
 @endsection
--- resources/views/emails/token_auth-plain.blade.php
+++ resources/views/emails/token_auth-plain.blade.php
@@ -1,13 +1,11 @@
 @extends('ig-common::layouts.email-plain')
 
 @section('content')
-@lang('ig-common::messages.email.hello')
-
 @lang('ig-user::token_auth.intro')
 
 
 <x-ig-common::emails.button-plain :link="$url">{{ __('ig-user::token_auth.action') }}</x-ig-common::emails.button-plain>
 
-@lang('ig-user::token_auth.expires', ['expires' => $expires])
+@lang('ig-user::token_auth.expires')
 
 @endsection
