a57250007474a554b2f3b316b3718703eb19c46c
George <george@internetguru.io>
2025-12-15 23:21:00 -1000

    Add autocomplete to inputs

--- resources/views/token-auth.blade.php
+++ resources/views/token-auth.blade.php
@@ -3,7 +3,7 @@
         <div class="card card-token-auth">
             <h2 class="display-6">@lang('ig-user::auth.token_auth.title')</h2>
             <x-ig::form :action="route('token-auth.form')" class="editable-skip">
-                <x-ig::input type="email" name="email" required>@lang('ig-user::auth.token_auth.email')</x-ig::input>
+                <x-ig::input type="email" name="email" autocomplete="email" required>@lang('ig-user::auth.token_auth.email')</x-ig::input>
                 <x-ig::submit>@lang('ig-user::auth.token_auth.submit')</x-ig::submit>
             </x-ig::form>
             <p class="mt-3 mb-0 text-end"><a href="{{ route('login') }}">@lang('ig-user::auth.back')</a></p>
