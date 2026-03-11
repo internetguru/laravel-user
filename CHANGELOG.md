27915d139af8bcafc0406f14880edfd2f7a770e2
petrzepav <pavel.petrzela@internetguru.cz>
2026-03-11 08:51:09 -0700

    Add Resend code link on PIN verify page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 - Redirect to PIN verify page after sending PIN email.
 - Simplify PIN email: remove button, show PIN and login link as plain text.
 - Stay on PIN verify page when PIN is invalid or expired.
+- Add "Resend code" link on PIN verify page.
 
 ## [6.1.1] - 2026-03-08
 
--- resources/views/pin-verify.blade.php
+++ resources/views/pin-verify.blade.php
@@ -6,6 +6,7 @@
                 <x-ig-user::pin-input name="pin" />
                 <x-ig::submit>@lang('ig-user::auth.pin_verify.submit')</x-ig::submit>
             </x-ig::form>
+            <p class="mt-3 mb-0"><a href="{{ route('pin-login') }}">@lang('ig-user::auth.pin_verify.resend')</a></p>
             <p class="mt-3 mb-0 text-end"><a href="{{ route('login') }}">@lang('ig-user::auth.back')</a></p>
         </div>
     </div>
