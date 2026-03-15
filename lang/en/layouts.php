d9d5b8679415abf55dc55fbe1c11319518a84548
petrzepav <pavel.petrzela@internetguru.cz>
2026-03-15 07:28:09 -0700

    Restore translations

--- lang/en/layouts.php
+++ lang/en/layouts.php
@@ -2,12 +2,18 @@
 
 return [
 
-    'login.description' => 'Select a provider or enter your email below. You can manage providers later.',
-    'login.title' => 'Log in or Register',
+    'login.description' => 'Log in with a provider or request a verification code.',
+    'login.title' => 'Log in with a provider',
     'login-demo.title' => 'Demo login',
-    'login-demo.description' => 'Demo login with pre-set users.',
-    'pin-verify.description' => 'Enter the PIN from your email to complete the login.',
-    'pin-verify.title' => 'PIN Verification',
+    'login-demo.description' => 'Log in with preset demo users.',
+    'register-email.description' => 'Create an account with your name and email.',
+    'register-email.title' => 'Create a new account manually',
+    'register.description' => 'Create an account with a provider or manually.',
+    'register.title' => 'Create a new account',
+    'pin-verify.description' => 'Enter the verification code from your email to complete the login.',
+    'pin-verify.title' => 'Verification code',
+    'pin-login.description' => 'Enter your email and we will send you a verification code.',
+    'pin-login.title' => 'Log in with email',
     'users.index.description' => 'List of existing users and their basic information.',
     'users.index.title' => 'User List',
     'users.show.description' => 'Interactive user information with connected identities.',
